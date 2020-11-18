e3f49725451dc1fc4e305ac279a2a6d26e9028fb
André <amatos497@gmail.com>
2020-11-18 11:20:07 -0300

    migrando configurações para bootstrap.yml

--- peixe-classe/src/main/java/com/example/demo/ClasseApplication.java
+++ peixe-classe/src/main/java/com/example/demo/ClasseApplication.java
@@ -2,11 +2,10 @@ package com.example.demo;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ClasseApplication {
