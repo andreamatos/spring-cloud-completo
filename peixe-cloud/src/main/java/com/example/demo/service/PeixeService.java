5df43b8a2685afc375ad825267de0d485c20668c
André <amatos497@gmail.com>
2020-11-18 17:51:35 -0300

    incluindo Eureka discovery client

--- peixe-cloud/src/main/java/com/example/demo/service/PeixeService.java
+++ peixe-cloud/src/main/java/com/example/demo/service/PeixeService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,19 @@ public class PeixeService {
 	
 	@Autowired
 	private RestTemplate client;
+	
+	@Autowired
+	private DiscoveryClient eurekaClient;
 
 	public void verificaClassePeixe(PeixeDTO peixe) {
 		
 		ResponseEntity<InfoClassePeixeDTO> exchange = 
 				client
 				.exchange(("http://peixe-classe/classe/" + peixe.getCodigo()), HttpMethod.GET, null, InfoClassePeixeDTO.class);
+		
+		eurekaClient.getInstances("peixe-classe").stream().forEach(peixeClasse -> {
+			System.out.println("localhost:" + peixeClasse.getPort());	
+		});
 		
 		System.out.println(exchange.getBody().getClassePeixe());
 	}
